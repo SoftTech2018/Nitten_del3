e3e0d9e7fb5f3e886dca093512e36bbb26cdf034
Moped <Moped@Jacob>
2015-05-10 17:13:38 +0200

    Tilføjet javadoc til IOperatoerDAO (mangler 3 sidste)

--- Nitten_cdio_del3/src/dtu/server/dal/dbconnection/daointerfaces/IOperatoerDAO.java
+++ Nitten_cdio_del3/src/dtu/server/dal/dbconnection/daointerfaces/IOperatoerDAO.java
@@ -7,13 +7,56 @@ import java.util.List;
 import dtu.shared.OperatoerDTO;
 
 public interface IOperatoerDAO {
+	/**
+	 * 
+	 * @param oprId
+	 * @return Returnerer et operatoer objekt
+	 * @throws DALException
+	 */
 	public OperatoerDTO getOperatoer(int oprId) throws DALException;
+	
+	/**
+	 * 
+	 * @return Returnerer liste af operatoerer
+	 * @throws DALException
+	 */
 	public List<OperatoerDTO> getOperatoerList() throws DALException;
+	
+	/**
+	 * 
+	 * @return Returnerer liste af operatoerer
+	 * @throws DALException
+	 */
 	public List<OperatoerDTO> getListViewOpr() throws DALException;
+	
+	/**
+	 * Opretter operatoer
+	 * @param opr
+	 * @throws DALException
+	 */
 	void createOperatoer(OperatoerDTO opr) throws DALException;
+	
+	/**
+	 * Ændrer på en nuværende operatoer
+	 * @param opr
+	 * @throws DALException
+	 */
 	void updateOperatoer(OperatoerDTO opr) throws DALException;
+	
+	/**
+	 * 
+	 * @return Returnerer operatoer view
+	 * @throws DALException
+	 */
 	public ResultSet getView() throws DALException;
+	
+	/**
+	 * 
+	 * @throws DALException
+	 */
 	public void callProcedure() throws DALException;
+	
+	
 	public void setFunction() throws DALException;
 	public String getFunction(int id) throws DALException, SQLException;
 	public void dropAll() throws DALException;
