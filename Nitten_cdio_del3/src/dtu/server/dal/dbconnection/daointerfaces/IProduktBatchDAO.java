258bbb281e26dcc6141a9af6d1b9905232064ea5
Moped <Moped@Jacob>
2015-05-10 17:15:21 +0200

    Tilføjelse: JavaDoc i IProduktBatchDAO

--- Nitten_cdio_del3/src/dtu/server/dal/dbconnection/daointerfaces/IProduktBatchDAO.java
+++ Nitten_cdio_del3/src/dtu/server/dal/dbconnection/daointerfaces/IProduktBatchDAO.java
@@ -5,8 +5,33 @@ import java.util.List;
 import dtu.server.dal.dbconnection.dto.ProduktBatchDTO;
 
 public interface IProduktBatchDAO {
+	
+	/**
+	 * 
+	 * @param pbId
+	 * @return Returnerer ProduktBatch
+	 * @throws DALException
+	 */
 	ProduktBatchDTO getProduktBatch(int pbId) throws DALException;
+	
+	/**
+	 * 
+	 * @return Returnerer liste med ProduktBatches
+	 * @throws DALException
+	 */
 	List<ProduktBatchDTO> getProduktBatchList() throws DALException;
+	
+	/**
+	 * Opretter en ProduktBatch
+	 * @param produktbatch
+	 * @throws DALException
+	 */
 	void createProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
+	
+	/**
+	 * Opdaterer en ProduktBatch
+	 * @param produktbatch
+	 * @throws DALException
+	 */
 	void updateProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
 }
