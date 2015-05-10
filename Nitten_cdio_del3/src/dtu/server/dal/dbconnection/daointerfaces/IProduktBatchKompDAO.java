ced7d74db7b9fa6fba2c0e23507468b0069ece59
Moped <Moped@Jacob>
2015-05-10 17:19:09 +0200

    Tilføjelse: JavaDoc til IProduktBatchKomponent

--- Nitten_cdio_del3/src/dtu/server/dal/dbconnection/daointerfaces/IProduktBatchKompDAO.java
+++ Nitten_cdio_del3/src/dtu/server/dal/dbconnection/daointerfaces/IProduktBatchKompDAO.java
@@ -5,10 +5,43 @@ import java.util.List;
 import dtu.server.dal.dbconnection.dto.ProduktBatchKompDTO;
 
 public interface IProduktBatchKompDAO {
+	
+	/**
+	 * 
+	 * @param pbId
+	 * @param rbId
+	 * @return Returnerer ProduktBatchKomponent
+	 * @throws DALException
+	 */
 	ProduktBatchKompDTO getProduktBatchKomp(int pbId, int rbId) throws DALException;
+	
+	/**
+	 * 
+	 * @param pbId
+	 * @return Returnerer bestemt ProduktBatchKomponent liste
+	 * @throws DALException
+	 */
 	List<ProduktBatchKompDTO> getProduktBatchKompList(int pbId) throws DALException;
+	
+	/**
+	 * 
+	 * @return Returnerer ProduktBatchKomponent liste
+	 * @throws DALException
+	 */
 	List<ProduktBatchKompDTO> getProduktBatchKompList() throws DALException;
+	
+	/**
+	 * Opretter ProduktBatchKomponent
+	 * @param produktbatchkomponent
+	 * @throws DALException
+	 */
 	void createProduktBatchKomp(ProduktBatchKompDTO produktbatchkomponent) throws DALException;
+	
+	/**
+	 * Opdater ProduktBatchKomponent
+	 * @param produktbatchkomponent
+	 * @throws DALException
+	 */
 	void updateProduktBatchKomp(ProduktBatchKompDTO produktbatchkomponent) throws DALException;	
 }
 
